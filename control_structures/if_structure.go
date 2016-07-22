7a183093ec49fb203a0439a9edc978f2b72762d5
Vikas Gupta <Vikas Gupta>
2016-07-22 15:38:21 +0530

    if structure commit

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,16 @@
 package control_structures
 
 func fizzBuzz(i int32) string {
-	return ""
+
+	if i%2==0{
+		return i;
+	}else if i%3 == 0 && i%5 ==0{
+		return "FizzBuzz"
+	}else if i%3 == 0{
+		return "Fizz"
+	}else if i%5 == 0{
+		return "Buzz"
+	}else {
+		return ""
+	}
 }
